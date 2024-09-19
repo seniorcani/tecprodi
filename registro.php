f751fa8772a6d9abf48d9fe5d6a0ed6caeceadbc
Saul <sa.fabian90@gmail.com>
2024-09-19 17:05:29 -0600

    agregado registro php

--- registro.php
+++ registro.php
@@ -2,7 +2,7 @@
 // Verificar si el formulario fue enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Mostrar un mensaje de éxito
-    echo "Formulario enviado correctamente. ¡Funciona!";
+    echo "Formulario enviado correctamente. ¡Funciona bien!";
 } else {
     // Si alguien accede al archivo sin enviar el formulario
     echo "Acceso no válido.";
